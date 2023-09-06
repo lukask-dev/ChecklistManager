5b21b7813c8eac6279fbe33807bc05384cc87bc9
Lukas <lukas.kallenbach@gmail.com>
2023-09-06 22:35:59 +0300

    gh pages setup

--- checklist-manager/src/App.tsx
+++ checklist-manager/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/ChecklistManager" element={<Home />} />
       </Routes>
     </Router>
   )
